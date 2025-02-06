7e403667a8acf8c7fd4633ab9c55d01365e23dd5
JustArmaan <singharmaanbrar@gmail.com>
2025-02-06 13:26:48 -0800

    create .gitignore and .gitattributes

---
+++ SeriesTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SeriesTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
